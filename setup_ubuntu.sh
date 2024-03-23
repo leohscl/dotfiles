9ce0876c30073c402804f19217074bf8c9c00919
lhenches <lhenches@kali>
2024-03-23 08:17:37 +0100

    updated setup script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,7 +2,12 @@
 # sudo apt-add-repository ppa:fish-shell/release-3
 # sudo apt update
 # sudo apt install fish
+# chsh -s $(which fish)
+# log in and out
 # fish_hybrid_key_bindings
+
+#curl https://sh.rustup.rs -sSf | sh
+#source ~/.cargo/env.fish
 
 # cargo install sccache
 # echo "[build]
@@ -12,14 +17,20 @@
 # cargo install ripgrep
 # cargo install fd-find
 # cargo install cargo-info
-# cargo install starship
-cargo install du-dust
-cargo install speedtest-rs
-cargo install wiki-tui
-cargo install mprocs
-cargo install zellij
-#
+# cargo install du-dust
+# cargo install speedtest-rs
+# cargo install wiki-tui
+# cargo install mprocs
+# cargo install zellij
+
+# starship
+# curl -sS https://starship.rs/install.sh | sh
+# echo "starship init fish | source" >> $HOME/.config/fish/config.fish
 # mkdir ~/.config/fish
 # echo "alias ls=\"exa\"" > $HOME/.config/fish/config.fish
 # echo "alias cat=\"bat\"" >> $HOME/.config/fish/config.fish
-# echo "starship init fish | source" >> $HOME/.config/fish/config.fish
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+# sudo rm -rf /opt/nvim
+# sudo tar -C /opt -xzf nvim-linux64.tar.gz
+# echo "alias vim=nvim" >> ~/.config/fish/config.fish
+# echo "fish_add_path /opt/nvim-linux64/bin" >> ~/.config/fish/config.fish
