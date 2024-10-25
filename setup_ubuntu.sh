5cbad549607f73f8b5fd6da7dbe1ec7a3eb6d153
leo <leo@work_laptop.com>
2024-10-25 10:57:11 +0200

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,3 +1,4 @@
+# echo "setxkbmap -option caps:swapescape" >> ~/.profile
 # fish
 # sudo apt-add-repository ppa:fish-shell/release-3
 # sudo apt update
@@ -6,10 +7,24 @@
 # log in and out
 # fish_hybrid_key_bindings
 
+# sudo apt install curl
+# nvim
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+# echo "source ~/.asdf/asdf.fish" >> ~/.config/fish/config.fish
+# mkdir -p ~/.config/fish/completions; ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+# asdf plugin add neovim
+# asdf install neovim latest
+# asdf global neovim latest
+# echo "alias vim=nvim" >> ~/.config/fish/config.fish
+#
+# rm -rf ~/.config/nvim
+# git clone https://github.com/leohscl/nvim ~/.config/nvim
+
+
 # curl https://sh.rustup.rs -sSf | sh
 # source ~/.cargo/env.fish
-#
-# sudo apt install build-essential pkg-config libssl
+
+# sudo apt install build-essential pkg-config
 # cargo install sccache
 # echo "[build]
 # rustc-wrapper = \"$HOME/.cargo/bin/sccache\"" > $HOME/.cargo/config.toml
@@ -27,20 +42,12 @@
 # starship
 # curl -sS https://starship.rs/install.sh | sh
 # echo "starship init fish | source" >> $HOME/.config/fish/config.fish
-# mkdir ~/.config/fish
+# more aliases
 # echo "alias ls=\"exa\"" > $HOME/.config/fish/config.fish
 # echo "alias cat=\"bat\"" >> $HOME/.config/fish/config.fish
-# sudo rm -rf /opt/nvim
-# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-# sudo tar -C /opt -xzf nvim-linux64.tar.gz
-# echo "alias vim=nvim" >> ~/.config/fish/config.fish
-# echo "fish_add_path /opt/nvim-linux64/bin" >> ~/.config/fish/config.fish
-#
-# i3²
-
-sudo apt install libsensors-dev
-cd
-git clone https://github.com/greshake/i3status-rust
-cd i3status-rust
-cargo install --path . --locked
-./install.sh
+# sudo apt install libsensors-dev libssl-dev
+# cd
+# git clone https://github.com/greshake/i3status-rust
+# cd i3status-rust
+# cargo install --path . --locked
+# ./install.sh
